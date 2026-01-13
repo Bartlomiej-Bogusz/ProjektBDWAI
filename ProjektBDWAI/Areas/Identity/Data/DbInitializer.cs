c7956d0f523272b0bdfbfcf6037de36454cde192
Bartlomiej-Bogusz <bartlomiej.bogusz1@gmail.com>
2026-01-13 18:27:34 +0100

    Added more cars to initial db, changed main page text

--- ProjektBDWAI/Areas/Identity/Data/DbInitializer.cs
+++ ProjektBDWAI/Areas/Identity/Data/DbInitializer.cs
@@ -23,6 +23,38 @@ public static class DbInitializer
                     Rok = 2022,
                     CenaZaDzien = 300,
                     Dostepny = true
+                },
+                new Samochod
+                {
+                    Marka = "Volkswagen",
+                    Model = "Tiroc",
+                    Rok = 2021,
+                    CenaZaDzien = 250,
+                    Dostepny = true
+                },
+                new Samochod
+                {
+                    Marka = "Honda",
+                    Model = "Civic",
+                    Rok = 2018,
+                    CenaZaDzien = 150,
+                    Dostepny = true
+                },
+                new Samochod
+                {
+                    Marka = "Volkswagen",
+                    Model = "Golf",
+                    Rok = 2024,
+                    CenaZaDzien = 160,
+                    Dostepny = true
+                },
+                new Samochod
+                {
+                    Marka = "Porsche",
+                    Model = "Macan",
+                    Rok = 2024,
+                    CenaZaDzien = 500,
+                    Dostepny = true
                 }
             );
             context.SaveChanges();
